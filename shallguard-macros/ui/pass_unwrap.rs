2db1de196ab73f5aa21fc2afb858dda2442359a8
Tomas Siegl <tomas.siegl@braiins.cz>
2026-09-03 13:57:34 +0200

    Revert "Add compile-time vacuity front line and oracle opt-out to verifies"
    This reverts commit 6ce17a6fb08a5f664a50b2db576a115519a9e556.

--- shallguard-macros/ui/pass_unwrap.rs
+++
@@ -1,9 +0,0 @@
-use shallguard_macros as shallguard;
-
-#[shallguard::verifies("REQ-ZZ-001")]
-#[test]
-fn unwrap_is_a_failure_path() {
-    "7".parse::<u32>().unwrap();
-}
-
-fn main() {}
--- shallguard-macros/ui/vacuous_constant_assert.rs
+++
@@ -1,9 +0,0 @@
-use shallguard_macros as shallguard;
-
-#[shallguard::verifies("REQ-ZZ-001")]
-#[test]
-fn constant_assertion() {
-    assert!(true);
-}
-
-fn main() {}
